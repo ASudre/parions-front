3e2f33c489d0141b58e897ea656d3c1424706d54
Léo Raspaud <leo.raspaud@decasoft.fr>
2015-08-01 11:00:11 +0200

    init

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -19,14 +19,8 @@ angular
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
+        templateUrl: 'views/login.html',
+        controller: 'loginController'
       })
       .otherwise({
         redirectTo: '/'
---
+++ app/scripts/controllers/loginController.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('parionsApp')
+  .controller('loginController',[ '$scope', function ($scope) {
+  	$scope.test = '' ; 
+}]);
