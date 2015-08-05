10adb24fa0984986751ef3b3bfc55ac2aaf67e5f
Léo Raspaud <leo.raspaud@decasoft.fr>
2015-08-05 23:16:19 +0200

    ajout toolbar

---
+++ app/scripts/controllers/toolbar.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name frontApp.controller:TranslateController
+ * @description
+ * # TranslateController
+ * Controller of the frontApp
+ */
+angular.module('parionsApp')
+  .controller('toolbarController',[ '$scope',function ($scope) {
+}]);
